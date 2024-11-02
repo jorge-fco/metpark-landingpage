41f11c0e6346228d32eeaed9ae9a2d29d43c2922
Jorge Fco. <jorge.fco.mend@gmail.com>
2024-11-02 07:25:43 -0600

    Add assets

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+/*
+	📁 APP --> JS
+	2024
+*/
+(function(){
+
+	// Pages
+	Pages = [
+		'.js-home',
+	];
+
+	//= CORE
+	APP = {
+		init: function(){
+			APP.GoogleMaps();
+		},
+		GoogleMaps: function(){
+			var _lat = 20.6676961;
+			var _lng = -103.4378923;
+			var _zoom = 15;
+			var _title = 'AUREA MÉXICO';
+			var _style = [ { "elementType": "geometry", "stylers": [ { "color": "#ebe3cd" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#523735" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#f5f1e6" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#c9b2a6" } ] }, { "featureType": "administrative.land_parcel", "elementType": "geometry.stroke", "stylers": [ { "color": "#dcd2be" } ] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#ae9e90" } ] }, { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#93817c" } ] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [ { "color": "#a5b076" } ] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#447530" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#f5f1e6" } ] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [ { "color": "#fdfcf8" } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "color": "#f8c967" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#e9bc62" } ] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry", "stylers": [ { "color": "#e98d58" } ] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry.stroke", "stylers": [ { "color": "#db8555" } ] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#806b63" } ] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] }, { "featureType": "transit.line", "elementType": "labels.text.fill", "stylers": [ { "color": "#8f7d77" } ] }, { "featureType": "transit.line", "elementType": "labels.text.stroke", "stylers": [ { "color": "#ebe3cd" } ] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [ { "color": "#b9d3c2" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#92998d" } ] } ];
+			var _image = '';
+
+			//= Google Maps
+			var map = new google.maps.Map(document.getElementById('map'), {
+				center: {lat: _lat, lng: _lng},
+				zoom: _zoom,
+				styles: _style,
+				draggable: true,
+				scaleControl: false,
+	   			scrollwheel: false,
+	   			navigationControl: false,
+	   			streetViewControl: false,
+	   			mapTypeControl: false,
+	   			gestureHandling: 'cooperative'
+			});
+
+			//= Marker
+			var marker1 = new google.maps.Marker({
+				position: new google.maps.LatLng(_lat, _lng),
+				map: map,
+				title: _title
+			});
+		}
+	};
+
+	//= RUN...
+	window.theme = APP;
+	window.onload = function() {
+		APP.init();
+	};
+})();
